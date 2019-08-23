43c2cf8bed7fdec79a8dec2c6c39ac9eda64e142
gywish@163.com <gywish@163.com>
2019-08-23 15:42:09 +0800

    binary test

---
+++ admin/src/test/java/com/mhyj/user/controller/UserAdminControllerTest.java
@@ -0,0 +1,16 @@
+package com.mhyj.user.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author MHYJ
+ * @date 2019/8/14 19:08
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserAdminControllerTest {
+
+}
